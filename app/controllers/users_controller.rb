10aee079c774fbc545befb7fdc552754d4204a49
kk-mandarin <74140194+kk-mandarin@users.noreply.github.com>
2021-04-06 22:55:01 +0900

    view_一覧_test1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def index
     @users = User.order('created_at DESC')
     @trainings = Training.all
@@ -7,28 +8,34 @@ class UsersController < ApplicationController
     @max = Max.new
     @limits = Limit.all
     @limit = Limit.new
+
+def new
+  @users = User.new
+end
+
+def create
+  @user = User.new(user_params)
+  if @user.save
+    redirect_to root_path
+  else
+    render :new
   end
+end
 
   def new
     @users = User.new
   end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to root_path
-    else
-      render :new
+    def show
     end
-
-    def show; end
 
     def edit
       @max = Max.find(params[:id])
       @trainings = Training.all
     end
 
-    def update; end
+    def update
+    end
   end
 
   private
